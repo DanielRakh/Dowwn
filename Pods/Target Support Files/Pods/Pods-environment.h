1e89e7ab60d05582f7435bbb77b65f0c312f938a
Daniel Rakhamimov <danielrak@me.com>
2014-09-29 01:20:49 -0400

    CocoaPods + Pod: UIColor + Hex

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// UIColor+Additions
+#define COCOAPODS_POD_AVAILABLE_UIColor_Additions
+#define COCOAPODS_VERSION_MAJOR_UIColor_Additions 1
+#define COCOAPODS_VERSION_MINOR_UIColor_Additions 0
+#define COCOAPODS_VERSION_PATCH_UIColor_Additions 1
+
